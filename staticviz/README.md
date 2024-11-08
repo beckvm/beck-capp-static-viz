d076f23bee2a957e88bf499d7737ecac89356859
Tori Beck <toribeck@Toris-Macbook.local>
2024-11-07 19:52:57 -0600

    finished static visualization project

---
+++ staticviz/README.md
@@ -0,0 +1,16 @@
+## Does Money Talk? An exploration of budget allocations and how they are (or are not) reflected in day-to-day services in Chicago
+Tori Beck
+CAPP 30239: Data Visualization for Policy Analysis
+
+### Project Description
+This project seeks to briefly explore the city of Chicago's budget and the ways in which residents might see this money at work.
+Transparency in city finances is important to both efficiency and trust in city services and departments.  While the city's yearly budgets are
+free to view online, it is harder to understand where this money is going and how it might relate to everyday services such as the library or road
+maintenence.  These data visualizations explore where there might be disparities between where money is used vs how much services improve or expand and which city services see more or less funding than is necessary.  
+
+### Infographic
+an embedded screenshot of your final project
+
+### Data Sources
+All data is from the Chicago Data Portal https://data.cityofchicago.org.
+It is free to use without license.
--- staticviz/data/clean_library.py
+++ staticviz/data/clean_library.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-def clean_library_data():
+def clean_library_visitors_data():
     """
-    Clean and combine budget data
+    Clean and combine library visitors data
     """
     visitors_2019 = pd.read_csv("library_data/2019_library_visitors.csv")
     visitors_2020 = pd.read_csv("library_data/2020_library_visitors.csv")
@@ -31,7 +31,43 @@ def clean_library_data():
 
     visitors.to_csv("./library_visitors.csv", index = False)
 
-clean_library_data()
+clean_library_visitors_data()
+
+def clean_library_circulation_data():
+    """
+    Clean and combine budget data
+    """
+    circulation_2019 = pd.read_csv("library_data/2019_library_circulation.csv")
+    circulation_2020 = pd.read_csv("library_data/2020_library_circulation.csv")
+    circulation_2021 = pd.read_csv("library_data/2021_library_circulation.csv")
+    circulation_2022 = pd.read_csv("library_data/2022_library_circulation.csv")
+    circulation_2023 = pd.read_csv("library_data/2023_library_circulation.csv")
+
+
+    circulation_2019["year"] = "2019"
+    circulation_2019 = circulation_2019.rename(columns = {"LOCATION": "BRANCH"})
+    circulation_2020["year"] = "2020"
+    circulation_2020 = circulation_2020.drop(columns = "LOCATION") 
+    circulation_2021["year"] = "2021"
+    circulation_2021 = circulation_2021.drop(columns = "Location") 
+    circulation_2022["year"] = "2022"
+    circulation_2022 = circulation_2022.drop(columns = "Location") 
+    circulation_2023["year"] = "2023"
+    circulation_2023 = circulation_2023.drop(columns = "Location") 
+
+    circulation = pd.concat([circulation_2019, circulation_2020, circulation_2021, circulation_2022, circulation_2023],
+                        ignore_index = True, axis = 0)
+
+    circulation = pd.melt(circulation, id_vars = ["BRANCH", "year"], 
+                        value_vars = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 
+                            'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 
+                            'DECEMBER'], var_name = "month", value_name = "circulation_num")
+
+    circulation.to_csv("./library_circulation.csv", index = False)
+
+clean_library_circulation_data()
+
+
 
 
 
